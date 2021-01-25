c59e5101911e2a8d63ba95de163782a5d5d0a67e
Anay Wadhera <awadhera@berkeley.edu>
2021-01-25 12:46:05 -0800

    recovery: display current filesystem when going to format
    Change-Id: I98dfde91c069751144842943ad2af1f6f0c02bb1

--- recovery.cpp
+++ recovery.cpp
@@ -66,6 +66,7 @@
 #include "recovery_utils/roots.h"
 #include "volclient.h"
 
+using android::fs_mgr::Fstab;
 using android::volmgr::VolumeManager;
 using android::volmgr::VolumeInfo;
 
@@ -186,9 +187,15 @@ bool ask_to_continue_downgrade(Device* device) {
 }
 
 std::string get_preferred_fs(Device* device) {
-  std::vector<std::string> headers{ "Choose what filesystem you want to use on /data", "Entries here are supported by your device." };
-  std::vector<std::string> items = get_data_fs_items();
+  Fstab fstab;
+  auto read_fstab = ReadFstabFromFile("/etc/fstab", &fstab);
+  std::vector<std::string> headers{ "Choose what filesystem you want to use on /data", "Entries here are supported by your device.\n" };
   std::string fs = volume_for_mount_point("/data")->fs_type;
+  if (read_fstab) {
+      std::string current_filesystem = android::fs_mgr::GetEntryForPath(&fstab, "/data")->fs_type;
+      headers.emplace_back("Current filesystem: " + current_filesystem);
+  }
+  std::vector<std::string> items = get_data_fs_items();
 
   if (items.size() > 1) {
       size_t chosen_item = device->GetUI()->ShowMenu(
